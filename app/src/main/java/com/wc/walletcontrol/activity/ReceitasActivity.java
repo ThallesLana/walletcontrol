7b8be0e8133bf864bce9c15a25e09c0c1fd40c87
ThallesLana <thalles.lana1@gmail.com>
2021-11-09 20:04:17 -0300

    adding recipe

--- app/src/main/java/com/wc/walletcontrol/activity/ReceitasActivity.java
+++ app/src/main/java/com/wc/walletcontrol/activity/ReceitasActivity.java
@@ -1,16 +1,136 @@
 package com.wc.walletcontrol.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 import com.wc.walletcontrol.R;
+import com.wc.walletcontrol.config.ConfiguracaoFirebase;
+import com.wc.walletcontrol.helper.Base64Custom;
+import com.wc.walletcontrol.helper.DateCustom;
+import com.wc.walletcontrol.model.Movimentacao;
+import com.wc.walletcontrol.model.Usuario;
 
 public class ReceitasActivity extends AppCompatActivity {
+    private TextInputEditText campoData, campoCategoria, campoDescricao;
+    private EditText campoValor;
+    private Movimentacao movimentacao;
+    private DatabaseReference firebaseRef = ConfiguracaoFirebase.getFirebaseDatabase();
+    private FirebaseAuth autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+    private Double receitaTotal;
+    private Double receitaGerada;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receitas);
+
+        // Utilizando findViewById
+        campoValor = findViewById(R.id.editValor);
+        campoData = findViewById(R.id.editData);
+        campoCategoria = findViewById(R.id.editCategoria);
+        campoDescricao = findViewById(R.id.editDescricao);
+
+        //configuração do campo data atual
+        campoData.setText(DateCustom.dataAtual());
+        recuperarReceitaTotal();
+
     }
+
+    // Validação de preenchimento dos campos de receita
+    public Boolean validarcamposReceita(){
+
+        String textoValor = campoValor.getText().toString();
+        String textoData = campoData.getText().toString();
+        String textoCategoria = campoCategoria.getText().toString();
+        String textoDescricao = campoDescricao.getText().toString();
+
+        if ( !textoValor.isEmpty() ){
+            if ( !textoData.isEmpty() ){
+                if ( !textoCategoria.isEmpty() ){
+                    if ( !textoDescricao.isEmpty() ){
+                        return true;
+                    }else {
+                        Toast.makeText(ReceitasActivity.this, "Descrição não foi preenchida!", Toast.LENGTH_SHORT).show();
+                        return false;
+                    }
+                }else {
+                    Toast.makeText(ReceitasActivity.this, "Categoria não foi preenchida!", Toast.LENGTH_SHORT).show();
+                    return false;
+                }
+            }else {
+                Toast.makeText(ReceitasActivity.this, "Data não foi preenchida!", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        }else {
+            Toast.makeText(ReceitasActivity.this, "Valor não foi preenchido!", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+    }
+
+    public void salvarReceita(View view){
+
+        if (validarcamposReceita()){
+
+            movimentacao = new Movimentacao();
+            String data = campoData.getText().toString();
+            Double valorRecuperado = Double.parseDouble(campoValor.getText().toString());
+
+            movimentacao.setValor(valorRecuperado);
+            movimentacao.setCategoria(campoCategoria.getText().toString());
+            movimentacao.setDescricao(campoDescricao.getText().toString());
+            movimentacao.setData(data);
+            movimentacao.setTipo("d");
+
+            receitaGerada = valorRecuperado;
+            Double receitaAtualizada = receitaTotal + receitaGerada;
+            atualizarReceita(receitaAtualizada);
+            movimentacao.salvar(data);
+
+        }
+
+    }
+
+    // metodo receita total
+    public void recuperarReceitaTotal(){
+
+        String emailUsuario = autenticacao.getCurrentUser().getEmail();
+        String idUsuario = Base64Custom.codificarBase64( emailUsuario );
+        DatabaseReference usuarioRef = firebaseRef.child("Usuario").child(idUsuario);
+
+        usuarioRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Usuario usuario = snapshot.getValue( Usuario.class );
+                receitaTotal = usuario.getReceitaTotal();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    // atualizar receita
+    public void atualizarReceita(Double receita){
+
+        String emailUsuario = autenticacao.getCurrentUser().getEmail();
+        String idUsuario = Base64Custom.codificarBase64( emailUsuario );
+        DatabaseReference usuarioRef = firebaseRef.child("Usuario").child(idUsuario);
+
+        usuarioRef.child("receitaTotal").setValue(receita);
+    }
+
 }
