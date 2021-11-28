58240084744a98d1ec534f318ef24901cd1fe50a
ThallesLana <thalles.lana1@gmail.com>
2021-11-28 19:14:14 -0300

    Finalizing the project and publishing as alpha

--- app/src/main/java/com/wc/walletcontrol/model/Movimentacao.java
+++ app/src/main/java/com/wc/walletcontrol/model/Movimentacao.java
@@ -13,6 +13,7 @@ public class Movimentacao {
     private String descricao;
     private String tipo;
     private double valor;
+    private String key;
 
     public Movimentacao() {
     }
@@ -30,6 +31,14 @@ public class Movimentacao {
                 .setValue(this);
 
 
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
     }
 
     public String getData() {
